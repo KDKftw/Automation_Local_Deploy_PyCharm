2b0efae18f3607f7c36c894848a43600345b99e4
KDKftw <ooo.kadoun@gmail.com>
2022-04-02 00:46:31 +0200

    no message

--- poznavacky.py
+++ poznavacky.py
@@ -54,46 +54,13 @@ class TestPoznavacky_D(unittest.TestCase):
 
     def test_poznavacky_vikendy_D(self):
         self.driver.get(URL_poznavacky_vikendy)
-
         acceptConsent(self.driver)
         poznavacky_check_D(self, self.driver)
 
     def test_poznavacky_rodiny_D(self):
         self.driver.get(URL_poznavacky_rodiny)
-
         acceptConsent(self.driver)
-        self.driver.maximize_window()
-        self.driver.execute_script("window.scrollTo(0, 1080);")
-
-        time.sleep(6)
-        imgs = self.driver.find_elements_by_xpath("//*[@class='f_tile-image-content']")
-        assert imgs[0].is_displayed() == True
-        print(imgs)
-        x = 0
-        for _ in imgs:
-            imgsDisplayed = imgs[x].is_displayed()
-            x = x + 1
-
-            assert imgsDisplayed == True
-            print("true imgdisplay")
-
-        gridItems = self.driver.find_elements_by_xpath("//*[@class='f_tileGrid-item']")
-        y = 0
-        assert gridItems[0].is_displayed() == True
-        for _ in gridItems:
-            gridItemDisplayed = gridItems[y].is_displayed()
-            assert gridItemDisplayed == True
-            y = y + 1
-            print("grid true")
-
-        gridBig = self.driver.find_elements_by_xpath("//*[@class='f_tileGrid']")
-        assert gridBig[0].is_displayed() == True
-        a = 0
-        for _ in gridBig:
-            gridBigDisplayed = gridBig[a].is_displayed()
-            assert gridBigDisplayed == True
-            a = a + 1
-            print("big grid ture")
+        poznavacky_check_D(self, self.driver)
 
     def test_poznavacky_zazitky_D(self):
         self.driver.get(URL_poznavacky_zazitky)
