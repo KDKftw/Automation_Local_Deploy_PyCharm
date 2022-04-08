946898410385ec9b1e0941957277ffbad1551e6d
KDKftw <ooo.kadoun@gmail.com>
2022-04-08 21:18:44 +0200

    detske kluby edit

--- DetskeKluby_D.py
+++ DetskeKluby_D.py
@@ -1,6 +1,8 @@
-from to_import import acceptConsent, URL_kluby, setUp, tearDown
+from to_import import acceptConsent, URL_kluby, setUp, tearDown, generalDriverWaitImplicit
 import unittest
 import pyautogui as p
+import time
+
 p.FAILSAFE = False
 
 
@@ -17,6 +19,7 @@ class TestDetskeKluby_D(unittest.TestCase):
         self.driver.get(URL_kluby)
         acceptConsent(self.driver)
         self.driver.maximize_window()
+        generalDriverWaitImplicit(self.driver)
         benefitItem = self.driver.find_elements_by_xpath("//*[@class='f_benefit-item splide__slide']")
         assert benefitItem[0].is_displayed() == True
         a=0
@@ -25,8 +28,11 @@ class TestDetskeKluby_D(unittest.TestCase):
             a=a+1
             assert benefitItemDisplay == True
             print("benefit item")
-        p.press("pagedown", presses=3)
+        #p.press("pagedown", presses=3)
+        generalDriverWaitImplicit(self.driver)
+
         gridContainer = self.driver.find_elements_by_xpath("//*[@class='grd-container']")
+        self.driver.execute_script("arguments[0].scrollIntoView();", gridContainer[0])
         b=0
         assert gridContainer[0].is_displayed() == True
         for _ in gridContainer:
@@ -34,8 +40,10 @@ class TestDetskeKluby_D(unittest.TestCase):
             assert  gridContainerDisplay == True
             b=b+1
             print ("grind container")
-        p.press("pagedown", presses=2)
+        #p.press("pagedown", presses=2)
         tileImg = self.driver.find_elements_by_xpath("//*[@class='f_tile-image']")
+        kartyHoteluBottom = self.driver.find_element_by_xpath("//*[@class='f_tile f_tile--tour']")
+        self.driver.execute_script("arguments[0].scrollIntoView();", kartyHoteluBottom)
         c=0
         assert tileImg[0].is_displayed() == True
         for _ in tileImg:
@@ -43,3 +51,5 @@ class TestDetskeKluby_D(unittest.TestCase):
             assert tileImgDisplay == True
             c=c+1
             print("tile img")
+
+        
